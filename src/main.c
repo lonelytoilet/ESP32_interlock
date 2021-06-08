fcc9fbd7767ac3b08ed71d2b884383e4db19337e
lonelytoilet <seantmdonaldson94@gmail.com>
2021-06-08 00:16:33 -0400

    functional w/o powersaving

--- src/main.c
+++ src/main.c
@@ -30,20 +30,25 @@ https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/index.
 #include <esp32/pm.h> 
 #include <esp_sleep.h>
 //
-//#define MQTT_BROKER_IP "192.168.1.229"
+
+#define WIFI_SSID "ATTrhJ4FWa"
+#define WIFI_PASSWORD "id%astyfg8e5"
+#define MQTT_BROKER_IP "192.168.1.229"
+#define MQTT_TOPIC "esp32"
+/*
+#define WIFI_SSID "dd-wrt"
+#define WIFI_PASSWORD "interlock"
+#define MQTT_BROKER_IP "192.168.1.116"  //WPRG Raspberry PI IP
 #define MQTT_TOPIC "shellies/shelly1-E8DB84D6DA26/relay/0/command"  //topic to send commands to SH1
-//#define MQTT_TOPIC "esp32"
+*/
+#define PIN_IN GPIO_NUM_25 //GPIO_NUM_13
 #define DOOR_OPEN "open"
 #define DOOR_SHUT "shut"
-//#define WIFI_SSID "ATTrhJ4FWa"
-//#define WIFI_PASSWORD "id%astyfg8e5"
 #define TAG "ESP32_MQTT_CLIENT"
 #define MANAGE "manage_esp"
 #define SLEEP "enter_sleep"
 #define TRIP_SH1 "off" // command to open the shelly one
-#define WIFI_SSID "dd-wrt"
-#define WIFI_PASSWORD "interlock"
-#define MQTT_BROKER_IP "192.168.1.116"  //WPRG Raspberry PI IP
+
 #define OLED_SCL 15
 #define OLED_SDA 4
 #define OLED_RST 16
@@ -55,6 +60,7 @@ bool PANIC;
 /* TODO
 ==========================
 set static IP
+use getchar to ask for wifi info
 OLED battery charge display -> capacitive touch?
 ADC for battery reading
     -> dump battery voltage reading into csv file on broker
@@ -98,27 +104,30 @@ void wifi_connect (void)
     ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config)); // set wifi IAW configuration
     ESP_ERROR_CHECK(esp_wifi_start());
     ESP_ERROR_CHECK(esp_wifi_connect());
-    ESP_ERROR_CHECK(esp_wifi_set_ps(WIFI_PS_MIN_MODEM)); // set power saving mode for wifi
+    
 }
 
-/*
-void user_input (void *arg)
+
+esp_mqtt_client_handle_t user_input (esp_mqtt_event_t **user_input)
 {   
-    printf("user_input\n");
-    int input;
-    input = (int) &arg;
-    printf("user input: %i\n", input);
-    if (input == )
+    esp_mqtt_event_t data_p;  //event type
+    data_p = *(esp_mqtt_event_t *)user_input;  //dereference and typecast void pointer
+    unsigned char char_count;  // can count character count, but can't access entier word due to library defining *data as onyl char.
+    char_count = data_p.total_data_len; 
+    printf("Word length: %i\n", char_count);
+    //
+    //char command_char = (char)*data_p.data;  //data variable in event structure only holds one char at a time?!         
+    //printf("Command val? %c\n", command_char);
+    //const char comp = 'c';
+    for(int val = 0; val <= char_count; ++val)
     {
-        printf("Entering deep sleep.\n");
-        esp_deep_sleep_start();
+        char command_char = (char)*data_p.data;  //data variable in event structure only holds one char at a time?!         
+        printf("Command val? %c\n", command_char);
     }
-    else
-    {
-        printf("Failed input...\n");
-    }   
+    esp_mqtt_client_handle_t client = data_p.client;
+    return client;
 }
-*/
+
 
 static void mqtt_event_handler (void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
 {
@@ -136,8 +145,11 @@ static void mqtt_event_handler (void *handler_args, esp_event_base_t base, int32
         break;
     case MQTT_EVENT_DATA:
         printf("RECEIVED DATA!\n");
-        printf("DATA UNHANDLED!\n");
-        //user_input(event_data);
+        esp_mqtt_event_t **input_pointer;
+        input_pointer = event_data;
+        esp_mqtt_client_handle_t client = user_input(input_pointer);
+        esp_mqtt_client_publish(client, MQTT_TOPIC, "ESP32 Recieved data!\n", 0, 1, 1); // publishes to topic
+        
         break;
     default:
         break;
@@ -153,7 +165,7 @@ esp_mqtt_client_handle_t mqtt_init (void)
     };
     vTaskDelay(10000 / portTICK_RATE_MS); // give wifi_connect time to establish a full connection
     esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_config); //initialize client
-    esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL); // register mqtt event with mqtt handler
+    esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, client); // register mqtt event with mqtt handler
     ESP_ERROR_CHECK(esp_mqtt_client_start(client)); // starts the client
     esp_mqtt_client_publish(client, MQTT_TOPIC, "ESP32 Connected.\n", 0, 1, 1); // publishes to topic
     int id = esp_mqtt_client_subscribe(client, MANAGE, 0);
@@ -179,6 +191,7 @@ void isr_task (void* client)
         {
             printf("ISR EVENT: Tripping Shelly1 relay!\n");
             esp_mqtt_client_publish(client, MQTT_TOPIC, TRIP_SH1, 0, 1, 1);
+            
         }
     }
 }
@@ -189,19 +202,19 @@ void gpio_initialize(void* client)
     // initialize gpio for interrupt service routine handling
     gpio_queue = xQueueCreate(10, sizeof(uint32_t));  // create queue to handle isr
     xTaskCreate(isr_task, "xTaskCreate\n", 2048, client, 10, &xHandle);  // create isr task
-    ESP_ERROR_CHECK(gpio_install_isr_service(ESP_INTR_FLAG_LEVEL2)); //install isr service to gpio
-    ESP_ERROR_CHECK(gpio_isr_handler_add(GPIO_NUM_13, isr_handler, NULL)); // create an isr handler
-    gpio_pad_select_gpio(GPIO_NUM_13);  // pad select for gpio?
-    ESP_ERROR_CHECK(gpio_set_direction(GPIO_NUM_13, GPIO_MODE_INPUT)); // set pin 13 as input
-    ESP_ERROR_CHECK(gpio_set_pull_mode(GPIO_NUM_13, GPIO_PULLUP_ONLY)); // enable pullup resistor in pin 13
-    ESP_ERROR_CHECK(gpio_set_intr_type(GPIO_NUM_13, GPIO_INTR_NEGEDGE)); // negitive edge interrupt service routine
-    ESP_ERROR_CHECK(gpio_intr_enable(GPIO_NUM_13));  // enable isr
+    ESP_ERROR_CHECK(gpio_install_isr_service(ESP_INTR_FLAG_LOWMED)); //install isr service to gpio
+    ESP_ERROR_CHECK(gpio_isr_handler_add(PIN_IN, isr_handler, NULL)); // create an isr handler
+    gpio_pad_select_gpio(PIN_IN);  // pad select for gpio?
+    ESP_ERROR_CHECK(gpio_set_direction(PIN_IN, GPIO_MODE_INPUT)); // set pin 13 as input
+    ESP_ERROR_CHECK(gpio_set_pull_mode(PIN_IN, GPIO_PULLUP_ONLY)); // enable pullup resistor in pin 13
+    ESP_ERROR_CHECK(gpio_set_intr_type(PIN_IN, GPIO_INTR_LOW_LEVEL)); // negitive edge interrupt service routine
+    ESP_ERROR_CHECK(gpio_intr_enable(PIN_IN));  // enable isr
 }
 
 
 void init_power_save()
 {
-    #if CONFIG_PM_ENABLE
+    #if CONFIG_PM_ENABLE  // power saving mode configuration structure
         esp_pm_config_esp32_t pm_conf = {
         .max_freq_mhz = CONFIG_ESP32_DEFAULT_CPU_FREQ_MHZ,
         .min_freq_mhz = CONFIG_ESP32_XTAL_FREQ,
@@ -209,10 +222,10 @@ void init_power_save()
         .light_sleep_enable = true,
     #endif
     };
-    //esp_sleep_enable_wifi_wakeup();
     esp_pm_configure(&pm_conf);  // power management configuration
     //dynamic frequency scaling enabled via esp-IDF Menuconfig
     #endif
+    ESP_ERROR_CHECK(esp_wifi_set_ps(WIFI_PS_MIN_MODEM)); // set power saving mode for wifi
 }
 
 
@@ -235,18 +248,30 @@ void initialize()
     nonvolatile_init(); // initialize storage
     wifi_init_sta();  // initialize wifi as station mode
     wifi_connect();  // connect to pre-defined wifi AP (access point)
-    init_power_save(); // intialize power saving modes
+    //init_power_save(); // intialize power saving modes
+    //NOTE: the effects of power saving are not noticable while the ESP32 is plugged in via usb
+    // this includes the effect on wifi latency and ISR speed
     esp_mqtt_client_handle_t client = mqtt_init();  // initialize MQTT protocall
     gpio_initialize(client);  // init general purpose input/output
 }
 
 
+/*
+int intro_func()
+{
+    char _ssid[];   //    /o
+    _ssid = getchar();
+}
+*/
+
+
 void app_main(void)
 {
+    //wifi_ssid, wifi_passw = intro_func();
     initialize();
     //
     while(1){
-        vTaskDelay(1000 / portTICK_RATE_MS); // prevents the task watchdog from causing a system reboot...
+        vTaskDelay(1500 / portTICK_RATE_MS); // prevents the task watchdog from causing a system reboot...
             //...due to task watchdog timer timeout.
     }
 }
